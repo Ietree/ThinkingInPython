4aa320833b49d2d95e967cd3b71ea642007232c3
Root <Dylansuns@hotmail.com>
2018-01-14 22:22:47 +0800

    提交新代码

---
+++ ThinkingInPython/function/higherfunction/filter.py
@@ -0,0 +1,29 @@
+def is_odd(n):
+    return n % 2 == 1
+
+L = list(filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15]))
+print(L)
+
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+def _not_divisible(n):
+    return lambda x : x % n > 0
+
+
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n), it)
+
+for n in primes():
+    if n < 1000:
+        print(n)
+    else:
+        break
--- ThinkingInPython/function/higherfunction/map-reduce.py
+++ ThinkingInPython/function/higherfunction/map-reduce.py
@@ -5,3 +5,14 @@ def f(x):
 # map将传入的函数依次作用到序列的每个元素，并把结果作为新的Iterator返回
 r = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 list(r)
+
+L = []
+for n in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    L.append(f(n))
+print(L)
+
+from functools import reduce
+def add(x, y):
+    return x + y
+
+reduce(add, [1, 3, 5, 7, 8])
